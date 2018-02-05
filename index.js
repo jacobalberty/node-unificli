c11c63618b1c0e71aa61c78e61c8edb1f2adf45d
Jacob Alberty <jacob.alberty@gmail.com>
2018-02-05 12:41:29 -0600

    Splitting the login code up from the action code to support multiple actions

--- index.js
+++ index.js
@@ -16,17 +16,27 @@ catch (err) {
     }
 }
 
+const actions = {
+    poe: {
+        func: setPoeMode
+    }
+}
+
 var called = process.argv.splice(0, process.execArgv.length + 2).join(' ');
-var args = process.argv
-if (args.length !== 3) {
-    console.log (`usage: ${called} <switch mac> <switch port> <poe mode>`);
+var action = process.argv.splice(0, 1)[0];
+
+if (actions[action] === undefined) {
+    console.log(`Supported actions: ${Object.keys(actions).join(', ')}`);
     return;
 }
-var mac = args[0];
-var port = args[1];
-var mode = args[2];
 
 var controller = new unifi.Controller(config.addr, config.port);
+
+controller.login(config.username, config.password, function(error) {
+    if (error)
+        throw error;
+    actions[action].func(called, process.argv);
+});
 
 function updateAccessDevice(controller, sites, accessDevice) {
     var changes = accessDevice.getChanges();
@@ -35,9 +45,14 @@ function updateAccessDevice(controller, sites, accessDevice) {
     }
 }
 
-controller.login(config.username, config.password, function(error) {
-    if (error)
-        throw error;
+function setPoeMode(called, args) {
+    if (args.length !== 3) {
+        console.log (`usage: ${called} poe <switch mac> <switch port> <poe mode>`);
+        return;
+    }
+    var mac = args[0];
+    var port = args[1];
+    var mode = args[2];
     controller.getAccessDevices('default', function(error, data) {
         if (error)
             throw error;
@@ -47,4 +62,4 @@ controller.login(config.username, config.password, function(error) {
 
         updateAccessDevice(controller, config.site, ad);
     }, mac);
-});
+}
