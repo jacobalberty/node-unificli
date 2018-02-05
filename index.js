1bb6186a4f3fa7de92a6537f6a6130f645e48606
Jacob Alberty <jacob.alberty@gmail.com>
2018-02-05 15:33:14 -0600

    Added setup module to generate ~/.unificli.json

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require("fs")
     , accessDevice = require("node-unifi-settings").accessDevice;
 
 const actions = {
+    setup: require("./setup"),
     poe: {
         login: true,
         func: setPoeMode
---
+++ setup.js
@@ -0,0 +1,85 @@
+const fs = require("fs")
+    , os = require("os")
+    , readline = require("readline")
+    , Writable = require('stream').Writable
+    , async = require("async");
+
+function setup(called, args) {
+    var filen=`${os.homedir()}/.unificli.json`;
+    var oldConf;
+    try {
+        oldConf = JSON.parse(fs.readFileSync(filen, "utf-8"));
+    } catch (err) { // just discard errors and make a blank object because we want to make a new file anyway
+        oldConf = {
+            username: 'admin',
+            password: 'changeme',
+            addr: 'unifi',
+            port: '8443',
+            site: 'default'
+        };
+    }
+    var readline = require('readline');
+
+    var muteableStdout = new Writable({
+        write: function(chunk, encoding, callback) {
+        if (!this.muted)
+            process.stdout.write(chunk, encoding);
+        callback();
+        }
+    });
+
+    muteableStdout.muted = false;
+
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: muteableStdout,
+        terminal: true
+    });
+
+    async.series([
+        (callback) => {
+            rl.question(`Username (${oldConf.username}): `, (username) => {
+                callback();
+                if (username !== '')
+                    oldConf.username = username;
+            });
+        },
+        (callback) => {
+            muteableStdout.muted = true;
+            process.stdout.write(`Password: `);
+            rl.question(``, (password) => {
+                console.log();
+                muteableStdout.muted = false;
+                if (password !== '')
+                    oldConf.password = password;
+                callback();
+            });
+        },
+        (callback) => {
+            rl.question(`Address (${oldConf.addr}): `, (addr) => {
+                if (addr !== '')
+                    oldConf.addr = addr;
+                callback();
+            });
+        },
+        (callback) => {
+            rl.question(`Port (${oldConf.port}): `, (port) => {
+                if (port !== '')
+                    oldConf.port = port;
+                callback();
+            });
+        },
+        (callback) => {
+            rl.question(`Site (${oldConf.site}): `, (site) => {
+                if (site !== '')
+                    oldConf.site = site;
+                callback();
+            });
+        },
+    ], () => {
+        fs.writeFileSync(filen, JSON.stringify(oldConf), 'utf-8')
+        rl.close();
+    });
+}
+
+module.exports = { func: setup };
