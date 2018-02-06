480b0a1fcdc2498d9949c76ffce3e8a0f4d902d7
Jacob Alberty <jacob.alberty@gmail.com>
2018-02-06 16:17:38 -0600

    Moved device.js to be device/ls action and added a seperate device action to be over any other device sub actions.

--- device.js
+++ device.js
@@ -1,85 +1,28 @@
 'use strict';
-const columnify = require('columnify')
-    , accessDevice = require('node-unifi-settings').accessDevice;
 
-var deps = { };
+var deps = { }
 
-var columns = {
-    _: {
-        name: 'name',
-        ip: 'ip',
-        mac: 'mac',
-        model: 'model',
-        version: 'version'
-    },
-    'ugw,usw': {
-        'data down': 'txstat',
-        'data up': 'rxstat'
-    },
-    uap: {
-        clients: 'num_sta',
-        channel: 'channel',
-    },
-    all: {
-        uptime: 'uptime'
-    }
+const actions = {
+    ls: "./device/ls"
 }
 
-function listAccessDevices(called, args) {
-    var controller = deps.controller;
-    var config = deps.config;
-
-    var type = 'all';
-    var coltype=type;
-    if (args.type) {
-        type = args.type;
-        switch(type) {
-            case 'usw':
-            case 'ugw':
-            case 'ugw,usw':
-            case 'usw,ugw':
-                coltype='ugw,usw';
-                break;
-        default:
-            coltype=type;
-        }
+function deviceRelay(called, argv) {
+    var action = argv._.splice(0, 1)[0]
+    called += ' device';
+    if (actions[action] === undefined) {
+        console.log(`Supported actions: ${Object.keys(actions).join(', ')}`);
+        return;
     }
-    var filtype = type;
-    var fcol = { };
-    Object.assign(fcol, columns._, columns[coltype])
-
-    controller.getAccessDevices('default', function(error, data) {
-        if (error)
-            throw error;
-        var devices = data[0];
-        var output = [ ];
-        var devObjs = [ ];
-        for(key in devices) {
-            var dev = new accessDevice(devices[key]);
-
-            dev.human = (args.human !== 'false');
-
-            if (filtype !== 'all') {
-                var types = filtype.split(',');
-                if (types.indexOf(dev.type) === -1)
-                   continue;
-            }
-
-            var tmp = { };
-            Object.keys(fcol).forEach(function(key, index) {
-                if (dev[fcol[key]]) {
-                    tmp[key] = dev[fcol[key]];
-                }
-            });
-            output.push(tmp)
-
-        }
-        console.log(columnify(output));
-    });
+    actions[action] = require(actions[action]);
+    if(actions[action].deps !== undefined) {
+        Object.assign(actions[action].deps, deps);
+    }
+    actions[action].func(called, argv);
+    return;
 }
 
 module.exports = {
     login: true,
-    func: listAccessDevices,
+    func: deviceRelay,
     deps: deps
 }
